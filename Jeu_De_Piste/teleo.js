793c1ace3702e6aa65ccd51021ff94961603424e
Yohan Jarnac <yohan.jarnac@hotmail.com>
2024-11-07 21:44:19 +0100

    Update teleo.js
    MAJ mdp

--- Jeu_De_Piste/teleo.js
+++ Jeu_De_Piste/teleo.js
@@ -6,8 +6,8 @@ mdpVille = "B2D2.A4B3/D2B3.C2C4/D4./C3.D5/D3E4.C5B2/B5D1.D4E4/C3C4.D2E3/E2E3.E5/
 function checkTeam() {
     let mdp = document.getElementById("mdp").value;
 
-    let mdp1 = (mdp === "mdp1")
-    let mdp2 = (mdp === "mdp2")
+    let mdp1 = (mdp.lower() === "frosties")
+    let mdp2 = (mdp.lower() === "chocapic")
 
     if (mdp1) {
         document.getElementById("toDecode").innerHTML = mdpRangueil
